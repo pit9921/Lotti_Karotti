a725536afb0129355a3c32886570b9d9dc3e9372
pit9921 <77811022+pit9921@users.noreply.github.com>
2022-10-17 21:39:04 +0200

    Add files via upload

--- Lotti_Karotti.py
+++ Lotti_Karotti.py
@@ -5,7 +5,7 @@ import streamlit as st
 import os
 import random
 from PIL import Image
-import pyautogui
+#import pyautogui
 
 imgExtension = ["png", "jpeg", "jpg"] #Image Extensions to be chosen from
 allImages = list()
@@ -57,14 +57,14 @@ st.markdown("""
 image = Image.open('./Images/' + str(randomImage))
 
 import time
-time.sleep(1.5)
+#time.sleep(1.5)
 st.image(image)
 
 
  
 if st.button("Nächste"):
-    pyautogui.hotkey("ctrl","F5")
-#     st.experimental_rerun()
+#    pyautogui.hotkey("ctrl","F5")
+     st.experimental_rerun()
 
 
 # Remove “Made with Streamlit” from bottom of app
